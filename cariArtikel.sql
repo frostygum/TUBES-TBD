df0a5b5cc74661c96be3e2334c37610e0270755a
Teresa Magira <54297926+tmagira@users.noreply.github.com>
2021-06-16 12:38:47 +0700

    SP cariArtikel

---
+++ cariArtikel.sql
@@ -0,0 +1,30 @@
+ALTER PROCEDURE cariArtikel
+	@parameter varchar(10),
+	@filter nvarchar(32)
+AS
+	IF(@parameter = 'judul')
+	BEGIN
+		SELECT judul 
+		FROM artikel 
+		WHERE judul LIKE '%' + @filter +'%'
+	END
+
+	ELSE IF(@parameter='kategori')
+	BEGIN
+		SELECT Artikel.IdArtikel , Judul
+		FROM artikelKategori 
+			JOIN kategori
+				ON artikelKategori.idKategori = kategori.idKategori
+			JOIN Artikel 
+				ON Artikel.IdArtikel = ArtikelKategori.IdArtikel
+		WHERE NamaKategori LIKE '%' + @filter +'%' 
+	END
+
+	ELSE IF(@parameter='penulis')
+	BEGIN
+		SELECT * 
+		FROM Artikel join [User]
+			ON Artikel.IdPenulis = [User].IdUser
+		WHERE NamaDepan LIKE '%' + @filter +'%'  OR
+			  NamaBelakang LIKE '%' + @filter +'%' 	
+	END
